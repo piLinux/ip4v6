f2e231e113cc84014fabb3f97d656f95e1248ac6
mahir <mahir.hasan@m.pilinux.me>
2020-09-11 08:40:21 +0200

    Commit 2: full code with dependencies

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/piLinux/ip4v6
+
+go 1.15
+
+require github.com/joho/godotenv v1.3.0
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+// main function
+func main() {
+	// Load environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatalln("$PORT must be set")
+	}
+
+	http.HandleFunc("/", HTTPHandler)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+// HTTPHandler function
+func HTTPHandler(w http.ResponseWriter, r *http.Request) {
+	resp := GetIP(r) + "\n"
+	w.Write([]byte(resp))
+}
+
+// GetIP gets IP address by reading off the forwarded-for
+// header (for proxies) and falls back to use the remote address
+func GetIP(r *http.Request) string {
+	ipAddress := r.RemoteAddr
+	forwardedAddress := r.Header.Get("X-Forwarded-For")
+
+	if forwardedAddress != "" {
+		//X-Forwarded-For
+		ipAddress = forwardedAddress //Single IP
+
+		//Array of IPs
+		ip := strings.Split(forwardedAddress, ", ")
+		if len(ip) > 1 {
+			ipAddress = ip[0] //First IP
+		}
+	}
+	return ipAddress
+}
